6165cc7befa1a3d4827452bceade4f8ce86bd7be
Gabriel <gabysolotorevsky100@gmail.com>
2025-04-03 08:26:46 -0300

    terminado2

--- src/scenes/codigo_abierto.tsx
+++ src/scenes/codigo_abierto.tsx
@@ -1,54 +1,154 @@
-import { Circle, Camera, Line, Rect,Img ,makeScene2D, Path, Txt, Layout } from "@motion-canvas/2d";
+import {
+  Camera,
+  Circle,
+  Img,
+  Layout,
+  Line,
+  makeScene2D,
+  Path,
+  Rect,
+  Txt,
+} from "@motion-canvas/2d";
 import {
   all,
   beginSlide,
   createRef,
   createRefArray,
-  waitUntil,
   Direction,
   slideTransition,
   useRandom,
+  waitUntil,
 } from "@motion-canvas/core";
 import book from "../assets/book-svgrepo-com.svg";
 import share from "../assets/share-circle-svgrepo-com.svg";
 import pen from "../assets/pen-nib-svgrepo-com.svg";
 
-export default  makeScene2D(function* (view) {
+export default makeScene2D(function* (view) {
   const title = createRef<Txt>();
-  view.add(<Txt ref={title}fontSize={150} opacity={0} fill={"white"} textWrap>Software de codigo abierto</Txt>)
+  view.add(
+    <Txt ref={title} fontSize={150} opacity={0} fill={"white"} textWrap>
+      Software de codigo abierto
+    </Txt>,
+  );
   const arrows = createRefArray<Line>();
-  
+
   yield* slideTransition(Direction.Left);
-  yield* all(title().position([0,-250],1),title().opacity(1,1));
+  yield* all(title().position([0, -250], 1), title().opacity(1, 1));
 
-  view.add(<Txt ref={title} fill={"whitesmoke"} opacity={0} position={[0,-350]}>Alternativa al software propietario</Txt>)
-  yield* title().opacity(1,1);
+  view.add(
+    <Txt ref={title} fill={"whitesmoke"} opacity={0} position={[0, -350]}>
+      Alternativa al software propietario
+    </Txt>,
+  );
+  yield* title().opacity(1, 1);
 
-  view.add(<Txt ref={title} fill={"whitesmoke"} opacity={0} position={[0,0]}>Puede ser</Txt>)
-  yield* title().opacity(1,1);
-  view.add(<Line ref={arrows} lineWidth={5} end={0}  stroke={"white"} points={[[0,0],[0,100],[-500,100],[500,100]]}></Line>)
-  view.add(<Line ref={arrows} lineWidth={5} end={0} endArrow stroke={"white"} points={[[-500,100],[-500,200]]}></Line>)
-  view.add(<Line ref={arrows} lineWidth={5} end={0} endArrow stroke={"white"} points={[[0,100],[0,200]]}></Line>)
-  view.add(<Line ref={arrows} lineWidth={5} end={0} endArrow stroke={"white"} points={[[500,100],[500,200]]}></Line>)
-
+  view.add(
+    <Txt ref={title} fill={"whitesmoke"} opacity={0} position={[0, 0]}>
+      Puede ser
+    </Txt>,
+  );
+  yield* title().opacity(1, 1);
+  view.add(
+    <Line
+      ref={arrows}
+      lineWidth={5}
+      end={0}
+      stroke={"white"}
+      points={[[0, 50], [0, 100], [-500, 100], [500, 100]]}
+    >
+    </Line>,
+  );
+  view.add(
+    <Line
+      ref={arrows}
+      lineWidth={5}
+      end={0}
+      endArrow
+      stroke={"white"}
+      points={[[-500, 100], [-500, 200]]}
+    >
+    </Line>,
+  );
+  view.add(
+    <Line
+      ref={arrows}
+      lineWidth={5}
+      end={0}
+      endArrow
+      stroke={"white"}
+      points={[[0, 100], [0, 200]]}
+    >
+    </Line>,
+  );
+  view.add(
+    <Line
+      ref={arrows}
+      lineWidth={5}
+      end={0}
+      endArrow
+      stroke={"white"}
+      points={[[500, 100], [500, 200]]}
+    >
+    </Line>,
+  );
 
   const svg = createRefArray<Img>();
-  view.add(<Img ref={svg} src={book} opacity={0} width={100} height={100} position={[-500,350]}></Img>)
-  view.add(<Img ref={svg} src={share} opacity={0} width={100} height={100} position={[0,350]}></Img>)
-  view.add(<Img ref={svg} src={pen} opacity={0} width={100} height={100} position={[500,350]}></Img>)
+  view.add(
+    <Img
+      ref={svg}
+      src={book}
+      opacity={0}
+      width={100}
+      height={100}
+      position={[-500, 350]}
+    >
+    </Img>,
+  );
+  view.add(
+    <Img
+      ref={svg}
+      src={share}
+      opacity={0}
+      width={100}
+      height={100}
+      position={[0, 350]}
+    >
+    </Img>,
+  );
+  view.add(
+    <Img
+      ref={svg}
+      src={pen}
+      opacity={0}
+      width={100}
+      height={100}
+      position={[500, 350]}
+    >
+    </Img>,
+  );
   const texto = createRefArray<Txt>();
-  view.add(<Txt ref={texto} fill={"white"} opacity={0} position={[-500,250]}>Estudiado</Txt>)
-  view.add(<Txt ref={texto} fill={"white"} opacity={0} position={[0,250]}>Compartido</Txt>)
-  view.add(<Txt ref={texto} fill={"white"} opacity={0} position={[500,250]}>Modificado</Txt>)
-  
-  yield* all(... arrows.map(ar => ar.end(1,1)));
+  view.add(
+    <Txt ref={texto} fill={"white"} opacity={0} position={[-500, 250]}>
+      Estudiado
+    </Txt>,
+  );
+  view.add(
+    <Txt ref={texto} fill={"white"} opacity={0} position={[0, 250]}>
+      Compartido
+    </Txt>,
+  );
+  view.add(
+    <Txt ref={texto} fill={"white"} opacity={0} position={[500, 250]}>
+      Modificado
+    </Txt>,
+  );
 
-  for (const [index,val] of texto.entries()){
-    yield* val.opacity(1,1);
-    yield* svg[index].opacity(1,1);
+  yield* all(...arrows.map((ar) => ar.end(1, 1)));
+
+  for (const [index, val] of texto.entries()) {
+    yield* val.opacity(1, 1);
+    yield* svg[index].opacity(1, 1);
   }
 
   yield* beginSlide("Final2");
-}
-
-);
+});
--- src/scenes/sistemas_empresariales.tsx
+++ src/scenes/sistemas_empresariales.tsx
@@ -1,81 +1,103 @@
-import { Circle, Camera, Line, Rect, makeScene2D, Path, Txt, Layout } from "@motion-canvas/2d";
+import {
+  Camera,
+  Circle,
+  Layout,
+  Line,
+  makeScene2D,
+  Path,
+  Rect,
+  Txt,
+} from "@motion-canvas/2d";
 import {
   all,
   beginSlide,
   createRef,
   createRefArray,
-  waitUntil,
   Direction,
   slideTransition,
   useRandom,
+  waitUntil,
 } from "@motion-canvas/core";
 
-export default  makeScene2D(function* (view) {
-  
+export default makeScene2D(function* (view) {
   yield* slideTransition(Direction.Left);
-  
-  const sistemas = <Txt opacity={0} fill={"white"} fontSize={150}>Sistemas empresariales</Txt>
+
+  const sistemas = (
+    <Txt opacity={0} fill={"white"} fontSize={150}>Sistemas empresariales</Txt>
+  );
   view.add(sistemas);
-  yield* all(sistemas.opacity(1,1),sistemas.position([0,-400],1));
-  const ar = <Line points={[[0,-325],[0,0]]} end={0} endArrow stroke={"white"} lineWidth={5} />;
+  yield* all(sistemas.opacity(1, 1), sistemas.position([0, -400], 1));
+  const ar = (
+    <Line
+      points={[[0, -325], [0, 0]]}
+      end={0}
+      endArrow
+      stroke={"white"}
+      lineWidth={5}
+    />
+  );
   view.add(ar);
-  yield* ar.end(1,1);
+  yield* ar.end(1, 1);
   const SOA = createRef<Txt>();
-  view.add(<Txt ref={SOA} opacity={0} fill={"white"} position={[0,50]}>SOA</Txt>);
-  yield* SOA().opacity(1,1);
+  view.add(
+    <Txt ref={SOA} opacity={0} fill={"white"} position={[0, 50]}>SOA</Txt>,
+  );
+  yield* SOA().opacity(1, 1);
   yield* beginSlide("Introduccion");
-yield* all(SOA().opacity(0,1),sistemas.opacity(0,1),ar.opacity(0,1));
+  yield* all(SOA().opacity(0, 1), sistemas.opacity(0, 1), ar.opacity(0, 1));
   const levels = createRefArray<Path>();
   const piramid = createRef<Camera>();
-  view.add(<Camera ref={piramid}>
-    <Path
-      ref={levels}
-      lineWidth={4}
-      stroke={"black"}
-      fill={"white"}
-      data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
-      position={[-100, -100]}
-      scale={0.25}
-      opacity={0}
-    >
-    </Path>
+  view.add(
+    <Camera ref={piramid}>
+      <Path
+        ref={levels}
+        lineWidth={4}
+        stroke={"black"}
+        fill={"white"}
+        data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
+        position={[-100, -100]}
+        scale={0.25}
+        opacity={0}
+      >
+      </Path>
 
-<Path
-      ref={levels}
-      lineWidth={4}
-      stroke={"black"}
-      opacity={0}
-      fill={"white"}
-      data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
-      position={[-100, 50]}
-      scale={0.25}
-    >
-    </Path>
+      <Path
+        ref={levels}
+        lineWidth={4}
+        stroke={"black"}
+        opacity={0}
+        fill={"white"}
+        data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
+        position={[-100, 50]}
+        scale={0.25}
+      >
+      </Path>
 
-    <Path
-      ref={levels}
-      lineWidth={4}
-      stroke={"black"}
-      opacity={0}
-      fill={"white"}
-      data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
-      position={[-100, 200]}
-      scale={0.25}
-    >
-    </Path>
+      <Path
+        ref={levels}
+        lineWidth={4}
+        stroke={"black"}
+        opacity={0}
+        fill={"white"}
+        data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
+        position={[-100, 200]}
+        scale={0.25}
+      >
+      </Path>
 
-    <Path
-      ref={levels}
-      lineWidth={4}
-      opacity={0}
-      stroke={"black"}
-      fill={"white"}
-      data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
-      position={[-100, 350]}
-      scale={0.25}
-    >
-    </Path>
-  </Camera>);
+      <Path
+        ref={levels}
+        lineWidth={4}
+        opacity={0}
+        stroke={"black"}
+        fill={"white"}
+        data="M25.508,12.011 L576.715,12.011 L576.715,584.766 L25.508,584.766 L25.508,12.011 z"
+        position={[-100, 350]}
+        scale={0.25}
+      >
+      </Path>
+    </Camera>,
+  );
 
   for (const lev of levels) {
     yield* lev.opacity(1, 0.3);
@@ -102,55 +124,153 @@ yield* all(SOA().opacity(0,1),sistemas.opacity(0,1),ar.opacity(0,1));
   yield* all(
     levels[3].data(
       "M25.508,12.011 L1176.715,12.011 L1176.715,584.766 L25.508,584.766 L25.508,12.011 z",
-      0.3 ,
+      0.3,
     ),
     levels[3].position([-175, 350], 1),
   );
 
-  
+  yield* levels[1].fill("gray", 1);
+  yield* piramid().zoom(2, 1);
 
+  yield* all(
+    levels[1].data(
+      "M25.508,12.011 L1576.715,12.011 L1576.715,584.766 L25.508,584.766 L25.508,12.011 z",
+      0.3,
+    ),
+    levels[1].position([-225, 50], 0.5),
+  );
 
-  yield* levels[1].fill("gray",1)
-  yield* piramid().zoom(2,1);  
-
-  yield* all(levels[1].data(
-    "M25.508,12.011 L1576.715,12.011 L1576.715,584.766 L25.508,584.766 L25.508,12.011 z",
-    0.3,
-  ), levels[1].position([-225, 50], 0.5))
-
-  view.add(<Txt ref={SOA} opacity={0} position={[-50,250]} fill={"white"}>Capa superior</Txt>)
-  yield* all(SOA().opacity(1,1),SOA().fontSize(100,1));
+  view.add(
+    <Txt ref={SOA} opacity={0} position={[-50, 250]} fill={"white"}>
+      Capa superior
+    </Txt>,
+  );
+  yield* all(SOA().opacity(1, 1), SOA().fontSize(100, 1));
   yield* beginSlide("ERP");
   piramid().remove();
   SOA().remove();
   const erp_rec = createRef<Rect>();
-  view.add(<Rect ref={erp_rec} fill={"white"} width={200} height={100}><Txt ref={SOA} fill={"black"}>ERP</Txt></Rect>)
-  yield* erp_rec().position([0,-400],1);
+  view.add(
+    <Rect ref={erp_rec} fill={"white"} width={200} height={100}>
+      <Txt ref={SOA} fill={"black"}>ERP</Txt>
+    </Rect>,
+  );
+  yield* erp_rec().position([0, -400], 1);
   const benef = createRefArray<Line>();
 
-  view.add(<Line ref={benef} points={[[0,-400],[0,-200]]} end={0} stroke={"white"} lineWidth={5} ></Line>)
-  view.add(<Line ref={benef} points={[[-500,-200],[500,-200]]} end={0} stroke={"white"} lineWidth={5} ></Line>)
-  view.add(<Line ref={benef} points={[[-500,-200],[-500,-100]]} endArrow end={0} stroke={"white"} lineWidth={5} ></Line>)
-  view.add(<Line ref={benef} points={[[500,-200],[500,-100]]} endArrow end={0} stroke={"white"} lineWidth={5} ></Line>)
+  view.add(
+    <Line
+      ref={benef}
+      points={[[0, -350], [0, -200]]}
+      end={0}
+      stroke={"white"}
+      lineWidth={5}
+    >
+    </Line>,
+  );
+  view.add(
+    <Line
+      ref={benef}
+      points={[[-500, -200], [500, -200]]}
+      end={0}
+      stroke={"white"}
+      lineWidth={5}
+    >
+    </Line>,
+  );
+  view.add(
+    <Line
+      ref={benef}
+      points={[[-500, -200], [-500, -100]]}
+      endArrow
+      end={0}
+      stroke={"white"}
+      lineWidth={5}
+    >
+    </Line>,
+  );
+  view.add(
+    <Line
+      ref={benef}
+      points={[[500, -200], [500, -100]]}
+      endArrow
+      end={0}
+      stroke={"white"}
+      lineWidth={5}
+    >
+    </Line>,
+  );
 
-  for (const ar of benef){
-    yield* ar.end(1,1);
-  } 
+  for (const ar of benef) {
+    yield* ar.end(1, 1);
+  }
 
-  view.add(<Rect ref={SOA} fill={"white"} opacity={0} width={200} height={100} position={[-500,0]}><Txt ref={SOA} fill={"black"}>SAP</Txt></Rect>)
-  yield* SOA().opacity(1,1);
-  view.add(<Rect ref={SOA} fill={"white"} opacity={0} width={200} height={100} position={[500,0]}><Txt ref={SOA} fill={"black"}>Oracle</Txt></Rect>)
-  yield* SOA().opacity(1,1);
-  
+  view.add(
+    <Rect
+      ref={SOA}
+      fill={"white"}
+      opacity={0}
+      width={200}
+      height={100}
+      position={[-500, 0]}
+    >
+      <Txt ref={SOA} fill={"black"}>SAP</Txt>
+    </Rect>,
+  );
+  yield* SOA().opacity(1, 1);
+  view.add(
+    <Rect
+      ref={SOA}
+      fill={"white"}
+      opacity={0}
+      width={200}
+      height={100}
+      position={[500, 0]}
+    >
+      <Txt ref={SOA} fill={"black"}>Oracle</Txt>
+    </Rect>,
+  );
+  yield* SOA().opacity(1, 1);
+
   const con_ar = createRefArray<Line>();
-  view.add(<Line ref={con_ar} points={[[-500,0],[-500,100],[-150,100]]} end={0} endArrow stroke={"white"} lineWidth={5} ></Line>)
-  view.add(<Line ref={con_ar} points={[[500,0],[500,100],[150,100]]} end={0} endArrow stroke={"white"} lineWidth={5} ></Line>)
+  view.add(
+    <Line
+      ref={con_ar}
+      points={[[-500, 50], [-500, 100], [-150, 100]]}
+      end={0}
+      endArrow
+      stroke={"white"}
+      lineWidth={5}
+    >
+    </Line>,
+  );
+  view.add(
+    <Line
+      ref={con_ar}
+      points={[[500, 50], [500, 100], [150, 100]]}
+      end={0}
+      endArrow
+      stroke={"white"}
+      lineWidth={5}
+    >
+    </Line>,
+  );
 
-  yield* all(... con_ar.map(arrow => arrow.end(1,1)));
+  yield* all(...con_ar.map((arrow) => arrow.end(1, 1)));
 
-  view.add(<Rect ref={SOA} fill={"white"} opacity={0} width={300} height={100} position={[0,100]}><Txt ref={SOA} fill={"black"}>Capacitacion</Txt></Rect>)
-  yield* SOA().opacity(1,1);
-  
+  view.add(
+    <Rect
+      ref={SOA}
+      fill={"white"}
+      opacity={0}
+      width={300}
+      height={100}
+      position={[0, 100]}
+    >
+      <Txt ref={SOA} fill={"black"}>Capacitacion</Txt>
+    </Rect>,
+  );
+  yield* SOA().opacity(1, 1);
+
   yield* beginSlide("Final");
-
 });
